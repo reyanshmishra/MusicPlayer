d27b94b9b8389adc1ce776b337f724f8af06637a
Deepak Vishwakarma <lilwaynedeepak@outlook.com>
2019-04-02 09:18:41 +0530

    ok

---
+++ app/src/main/java/app/deepakvishwakarma/com/musicplayer/Interface/PrepareServiceListener.java
@@ -0,0 +1,7 @@
+package app.deepakvishwakarma.com.musicplayer.Interface;
+
+import app.deepakvishwakarma.com.musicplayer.Services.MusicService;
+
+public interface PrepareServiceListener {
+    void onServiceRunning(MusicService musicService);
+}
